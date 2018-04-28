f2e5151cad12c7b4745532353c0f8cb466ac4ad0
NicoleHaha <80515552@qq.com>
2018-04-28 21:49:39 +0800

    将数据用表格展示

--- src/main/java/com/example/employeeapi/controller/EmployeeController.java
+++ src/main/java/com/example/employeeapi/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.example.employeeapi.controller;
 
 import com.example.employeeapi.datasource.EmployeeRepository;
+import com.example.employeeapi.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @EnableAutoConfiguration
@@ -17,8 +23,17 @@ public class EmployeeController {
 
     @RequestMapping("/employees")
     String getEmployees(Model model) {
-
-        model.addAttribute("message", "hello world");
+        List<Employee> employees = employeeRepository.getAllEmployees();
+        List<Map> list = new ArrayList<Map>();
+        for (Employee employee : employees) {
+            Map map = new HashMap();
+            map.put("id", employee.getId());
+            map.put("name", employee.getName());
+            map.put("age", employee.getAge());
+            map.put("gender", employee.getGender());
+            list.add(map);
+        }
+        model.addAttribute("message", list);
 
         return "employees";
     }
--- src/main/webapp/WEB-INF/jsp/employees.jsp
+++ src/main/webapp/WEB-INF/jsp/employees.jsp
@@ -1,5 +1,5 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-         pageEncoding="UTF-8" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
 <html>
@@ -7,9 +7,37 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>员工列表</title>
 </head>
+<style>
+body{
+text-align:center;
+font-weight:bold;
+font-size:20px
+}
+table{
+border-collapse:collapse;
+border:2px solid black;
+}
+th,tr,td{
+    height:55px;
+    border:2px solid black;
+}
+</style>
 <body>
-
-<div>${message}</div>
-
+<table style = "width:70%;cellspacing = 0px;cellpadding=0px">
+    <tr bgcolor = "#000000" style = "color:#fff" >
+        <th>编号</th>
+        <th>姓名</th>
+        <th>年龄</th>
+        <th>性别</th>
+    </tr>
+    <c:forEach items = "${message}" var = "employee" varStatus = "status">
+        <tr bgcolor="${status.index%2==0?'#F0F0F0':'#FFFFFF'}">
+            <td>${employee.id}</td>
+            <td>${employee.name}</td>
+            <td>${employee.age}</td>
+            <td>${employee.gender}</td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
